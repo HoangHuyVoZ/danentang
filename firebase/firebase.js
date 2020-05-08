8cc1ed7fe27b2c8d248d0ef5c25a1b04a76c13d4
HoangHuyVoZ <g4nh4t127@gmail.com>
2020-05-08 10:16:29 +0700

    update1

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyABVlQUFf52xJwZBkfknZq9dQnHFtoR4sM",
+  authDomain: "react-native-d573c.firebaseapp.com",
+  databaseURL: "https://react-native-d573c.firebaseio.com",
+  projectId: "react-native-d573c",
+  storageBucket: "react-native-d573c.appspot.com",
+  messagingSenderId: "843414531648",
+  appId: "1:843414531648:web:21d10cbf8d12d7affeb9a5",
+  measurementId: "G-84QKRWFYTY"
+};
+export default firebaseConfig = firebase.initializeApp(config);
